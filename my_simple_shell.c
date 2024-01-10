790c5b3280e8baa1a5fed6a745a24a3f6ce24db2
root <veresan72@gmail.com>
2024-01-10 14:52:29 -0800

    Finish

--- my_simple_shell.c
+++ my_simple_shell.c
@@ -5,6 +5,8 @@
 #include <signal.h>
 #include <string.h>
 
+extern char **environ;  // Declaration of environ variable
+
 #define MAX_COMMAND_LENGTH 100
 
 /**
@@ -13,8 +15,8 @@
  *    */
 void sigint_handler(int signum)
 {
-		(void)signum;
-			write(STDOUT_FILENO, "\n#cisfun$ ", 10);
+	    (void)signum;
+	        write(STDOUT_FILENO, "\n#cisfun$ ", 10);
 }
 
 /**
@@ -23,58 +25,56 @@ void sigint_handler(int signum)
  *    */
 void execute_command(char *command)
 {
-		char *args[2];
+	    char *args[2];
 
-			args[0] = command;
-				args[1] = NULL;
+	        args[0] = command;
+		    args[1] = NULL;
 
-					if (fork() == 0)
-							{
-										/* Child process */
-										if (execve(args[0], args, NULL) == -1)
-													{
-																	perror(command);
-																				exit(EXIT_FAILURE);
-																						}
-											}
-						/* Parent process */
-						/* (Note: Removed the unused 'status' variable) */
+		        if (fork() == 0)
+				    {
+					            /* Child process */
+					            if (execve(args[0], args, environ) == -1)
+							            {
+									                perror(command);
+											            exit(EXIT_FAILURE);
+												            }
+						        }
+			    /* Parent process */
+			    wait(NULL);
 }
 
 /**
- *  * main - Simple and Unique Shell
+ *  * main - Simple Shell 0.1
  *   *
  *    * Return: Always 0 (Success)
  *     */
 int main(void)
 {
-		char command[MAX_COMMAND_LENGTH];
+	    char command[MAX_COMMAND_LENGTH];
 
-			signal(SIGINT, sigint_handler);
+	        signal(SIGINT, sigint_handler);
 
-				while (1)
-						{
-									printf("#cisfun$ ");
-											fflush(stdout);
+		    while (1)
+			        {
+					        write(STDOUT_FILENO, "#cisfun$ ", 10);
+						        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+								        {
+										            write(STDOUT_FILENO, "\nGoodbye! 🌟\n", 15);
+											                break;
+													        }
 
-													if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
-																{
-																				printf("\nGoodbye! 🌟\n");
-																							break;
-																									}
+							        /* Remove the newline character at the end */
+							        command[strlen(command) - 1] = '\0';
 
-															/* Remove the newline character at the end */
-															command[strlen(command) - 1] = '\0';
+								        if (strcmp(command, "exit") == 0)
+										        {
+												            write(STDOUT_FILENO, "\nGoodbye! 🌟\n", 15);
+													                break;
+															        }
 
-																	if (strcmp(command, "exit") == 0)
-																				{
-																								printf("\nGoodbye! 🌟\n");
-																											break;
-																													}
+									        execute_command(command);
+										    }
 
-																			execute_command(command);
-																				}
-
-					return (0);
+		        return 0;
 }
 
