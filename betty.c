e8f7184a16965639ad9ab4e51bf7b51c43e95a79
root <veresan72@gmail.com>
2024-01-10 12:40:15 -0800

    Finish

---
+++ betty.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point of the program
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	    printf("Greetings, Universe!\n");
+	        return 0;
+}
+
---
+++ my_simple_shell.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <string.h>
+
+#define MAX_COMMAND_LENGTH 100
+
+/**
+ *  * sigint_handler - Ignore SIGINT (Ctrl+C) in the shell
+ *   * @signum: Signal number
+ *    */
+void sigint_handler(int signum)
+{
+	    (void)signum;
+	        write(STDOUT_FILENO, "\n#cisfun$ ", 10);
+}
+
+/**
+ *  * execute_command - Execute the given command
+ *   * @command: The command to execute
+ *    */
+void execute_command(char *command)
+{
+	    char *args[2];
+	        int status;
+
+		    args[0] = command;
+		        args[1] = NULL;
+
+			    if (fork() == 0)
+				        {
+						        /* Child process */
+						        if (execve(args[0], args, NULL) == -1)
+								        {
+										            perror(command);
+											                exit(EXIT_FAILURE);
+													        }
+							    }
+			        else
+					    {
+						            /* Parent process */
+						            wait(&status);
+							        }
+}
+
+/**
+ *  * main - Simple and Unique Shell
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	    char command[MAX_COMMAND_LENGTH];
+
+	        signal(SIGINT, sigint_handler);
+
+		    while (1)
+			        {
+					        write(STDOUT_FILENO, "#cisfun$ ", 10);
+						        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+								        {
+										            write(STDOUT_FILENO, "\nGoodbye! 🌟\n", 15);
+											                break;
+													        }
+
+							        /* Remove the newline character at the end */
+							        command[strlen(command) - 1] = '\0';
+
+								        if (strcmp(command, "exit") == 0)
+										        {
+												            write(STDOUT_FILENO, "\nGoodbye! 🌟\n", 15);
+													                break;
+															        }
+
+									        execute_command(command);
+										    }
+
+		        return 0;
+}
+
