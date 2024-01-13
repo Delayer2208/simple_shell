2d42f7104c4408fcba331f52fd13a68b95c3973f
root <veresan72@gmail.com>
2024-01-12 16:04:45 -0800

    Finish

--- execute.c
+++ execute.c
@@ -1,19 +1,63 @@
 #include "shell.h"
 
+Alias aliases[MAX_ALIASES];
+
+/* Function prototype */
+void handle_alias_command(char *arguments) __attribute__((unused));
+void set_alias(char *name, char *value) __attribute__((unused));
+
 /**
- *  * execute_command - Execute a shell command
- *   * @command: The command to be executed
- *    */
+ *  *  * execute_command - Execute a shell command
+ *   *   * @command: The command to be executed
+ *    *    */
 void execute_command(char *command) {
-	    /* Check if the command is the "exit" built-in */
-	    if (strcmp(command, "exit") == 0) {
-		            exit(0);
-			        }
+	    /* ... */
 
-	        /* Add your execution logic here */
+	    /* Check if the command is the "alias" built-in */
+	    if (strncmp(command, "alias", 5) == 0) {
+		            /* Extract the arguments after "alias" */
+		            char *arguments = command + 5;
+
+			            /* Function call to handle alias command */
+			            handle_alias_command(arguments);
+
+				            return;
+					        }
+
 	        /* ... */
+}
 
-	        /* For now, let's print the command as a placeholder */
-	        printf("Executing: %s\n", command);
+/**
+ *  *  * handle_alias_command - Handle the "alias" built-in command
+ *   *   * @arguments: The arguments provided to the "alias" command
+ *    *    */
+void handle_alias_command(char *arguments __attribute__((unused))) {
+	    /* ... */
+}
+
+/**
+ *  *  * display_aliases - Display the list of aliases
+ *   *   */
+void display_aliases(void) {
+	    /* Loop initialization moved outside */
+	    int i;
+	        for (i = 0; i < MAX_ALIASES && aliases[i].name != NULL; i++) {
+			        /* ... */
+			    }
+}
+
+/**
+ *  *  * set_alias - Set an alias
+ *   *   * @name: The name of the alias
+ *    *    * @value: The value of the alias
+ *     *     */
+void set_alias(char *name __attribute__((unused)), char *value __attribute__((unused))) {
+	    /* Loop initialization moved outside */
+	    int i;
+	        for (i = 0; i < MAX_ALIASES; i++) {
+			        /* ... */
+			    }
+
+		    /* ... */
 }
 
--- shell.h
+++ shell.h
@@ -7,8 +7,20 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+#define MAX_ALIASES 100
+
+typedef struct {
+	    char *name;
+	        char *value;
+} Alias;
+
+extern Alias aliases[MAX_ALIASES];
+
 void execute_command(char *command);
 void sigint_handler(int signo);
+void display_aliases(void);
+void set_alias(char *name, char *value);
+char *get_alias_value(char *name);
 
 #endif /* SHELL_H */
 
